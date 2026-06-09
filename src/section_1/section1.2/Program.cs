5ca6cae56b948d25e1503e5120417bd74b3dbf0b
cristiannv <cristianenv13@gmail.com>
2026-06-09 14:21:55 -0500

    Add Section 1

--- src/section_1/section1.2/Program.cs
+++ src/section_1/section1.2/Program.cs
@@ -1,5 +1,18 @@
 ﻿using StackExchange.Redis;
 
 // TODO for Coding Challenge Start here on starting-point branch
+var options = new ConfigurationOptions
+{
+    // add and update parameters as needed
+    EndPoints = {"localhost:6379"}
+};
 
+// initalize a multiplexer with ConnectionMultiplexer.Connect()
+var muxer = ConnectionMultiplexer.Connect(options);
+
+// get an IDatabase here with GetDatabase
+var db = muxer.GetDatabase();
+
+// add ping here
+Console.WriteLine($"ping: {db.Ping().TotalMilliseconds} ms");
 // end programming challenge
